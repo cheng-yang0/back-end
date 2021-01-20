7a504cd429aecfb504204ce9b1b1af243bec32e5
e <chengyang@linkdoc.com>
2021-01-20 12:39:36 +0800

    移除dist文件夹

--- dist/server/user/user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateUserDTO {
-    readonly _id: string;
-    readonly user_name: string;
-    readonly password: string;
-}
-export declare class EditUserDTO {
-    readonly user_name: string;
-    readonly password: string;
-}
